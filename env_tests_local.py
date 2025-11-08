906eb4f06f3b9a14f12ed7b82e6dcf005668933a
Christine J <c.christine.jacob@student.tue.nl>
2025-11-08 21:51:56 +0100

    Removing outdated files (2)

--- env_tests_local.py
+++
@@ -1,9 +0,0 @@
-import skpm.event_logs as event_logs
-
-# Get all attributes/classes in the module
-all_items = dir(event_logs)
-
-# Print the attributes/classes
-for item in all_items:
-    print(item)
-    
